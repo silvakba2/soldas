436e175a1c5bf5bc5b6f90482e1ce86cfe82b312
silvakba2 <139677782+silvakba2@users.noreply.github.com>
2023-07-23 00:41:06 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x0fcac4b9d9dd6c8f209651868eee162fe6765586";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
